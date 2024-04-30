357b0a3a35c673c0fa4cffed0544fbc279f35584
Andrei-Boghiu <vandrei1408@yahoo.com>
2024-04-30 22:10:17 +0300

    initial commit

---
+++ src/background.ts
@@ -0,0 +1,35 @@
+import puppeteer from 'puppeteer-core/lib/cjs/puppeteer/web';
+import {ExtensionDebuggerTransport} from '../lib';
+
+const run = async (tabId: number) => {
+  const extensionTransport = await ExtensionDebuggerTransport.create(tabId);
+  const browser = await puppeteer.connect({
+    transport: extensionTransport,
+    defaultViewport: null,
+  });
+
+  const [page] = await browser.pages();
+
+  await page.goto('https://wikipedia.org');
+
+  const englishButton = await page.waitForSelector('#js-link-box-en > strong');
+  await englishButton?.click();
+
+  const searchBox = await page.waitForSelector('#searchInput');
+  await searchBox?.type('computer science');
+  await page.keyboard.press('Enter');
+
+  await page.close();
+};
+
+chrome.commands.onCommand.addListener(command => {
+  if (command === 'start') {
+    chrome.tabs.create(
+      {
+        active: true,
+        url: 'https://www.google.co.in',
+      },
+      tab => (tab.id ? run(tab.id) : null)
+    );
+  }
+});
